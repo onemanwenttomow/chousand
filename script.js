eb55999147d561c05852ff78a27ab32fbf502068
Peter Anderson <pete.anderson@gmail.com>
2019-07-12 20:07:15 +0200

    wrapped in everything in a function that waits for the page to load

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
 console.log("sanity check");
 
-var vid = document.getElementById('bgvid');
-var playBtn = document.getElementById('btn');
-console.log(playBtn);
+document.addEventListener('DOMContentLoaded', function() {
+    var vid = document.getElementById('bgvid');
+    var playBtn = document.getElementById('btn');
+    console.log(playBtn);
 
-document.querySelector('h1').addEventListener("click", function(e) {
-    e.stopPropagation();
-    console.log("clicked");
-    vid.paused ? vid.play() : vid.pause();
-});
+    document.querySelector('h1').addEventListener("click", function(e) {
+        e.stopPropagation();
+        console.log("clicked");
+        vid.paused ? vid.play() : vid.pause();
+    });
 
-setTimeout(function(){
-    console.log("timeout");
-    if (vid.paused) {
-        vid.play();
-        vid.muted = true;
-    }
-}, 500);
+    setTimeout(function(){
+        console.log("timeout");
+        if (vid.paused) {
+            vid.play();
+            vid.muted = true;
+        }
+    }, 500);
 
-playBtn.addEventListener("click", function() {
-    if (vid.muted === false) {
-        vid.muted = true;
-        playBtn.innerHTML = '<i id="play" class="fas fa-volume-mute fa-4x"></i>';
-    } else {
-        vid.muted = false;
-        playBtn.innerHTML = '<i id="play" class="fas fa-volume-up fa-4x"></i>';
-    }
-});
+    playBtn.addEventListener("click", function() {
+        if (vid.muted === false) {
+            vid.muted = true;
+            playBtn.innerHTML = '<i id="play" class="fas fa-volume-mute fa-4x"></i>';
+        } else {
+            vid.muted = false;
+            playBtn.innerHTML = '<i id="play" class="fas fa-volume-up fa-4x"></i>';
+        }
+    });
 
-const dialog = document.getElementById('dialog');
-const openBtn = document.getElementById('tourbtn');
-const closeBtn = document.getElementById('closeBtn');
+    const dialog = document.getElementById('dialog');
+    const openBtn = document.getElementById('tourbtn');
+    const closeBtn = document.getElementById('closeBtn');
 
-openBtn.addEventListener('click', function() {
-    dialog.setAttribute('open', true);
-});
+    openBtn.addEventListener('click', function() {
+        dialog.setAttribute('open', true);
+    });
 
-closeBtn.addEventListener('click', function() {
-    dialog.removeAttribute('open');
-});
+    closeBtn.addEventListener('click', function() {
+        dialog.removeAttribute('open');
+    });
+
+}, false);
